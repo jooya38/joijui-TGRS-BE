6eefe51f8786b3d2d18b71b5f938d0ad73075134
inqueue0979 <inqueue0979@gmail.com>
2024-08-04 20:25:32 +0900

    Added Proxy Server with Flask

---
+++ proxy.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+import sqlite3
+import os
+from create_db import init_db  # 데이터베이스 초기화 함수 가져오기
+
+app = Flask(__name__)
+
+# 데이터베이스 초기화
+if not os.path.exists('site.db'):
+    init_db()
+
+def get_db_connection():
+    conn = sqlite3.connect('site.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def home():
+    return "Hello, Flask with SQLite!"
+
+@app.route('/sites', methods=['GET'])
+def get_sites():
+    conn = get_db_connection()
+    data = conn.execute('SELECT * FROM sites').fetchall()
+    conn.close()
+    return jsonify([dict(row) for row in data])
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    conn = get_db_connection()
+    data = conn.execute('SELECT * FROM users').fetchall()
+    conn.close()
+    return jsonify([dict(row) for row in data])
+
+@app.route('/reviews', methods=['GET'])
+def get_reviews():
+    conn = get_db_connection()
+    data = conn.execute('SELECT * FROM reviews').fetchall()
+    conn.close()
+    return jsonify([dict(row) for row in data])
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    new_user = request.get_json()
+    conn = get_db_connection()
+    conn.execute('''
+        INSERT INTO users (user_id, name, phone, email, password, points)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (new_user['user_id'], new_user['name'], new_user['phone'], new_user['email'], new_user['password'], int(new_user['points'])))
+    conn.commit()
+    conn.close()
+    return '', 201
+
+@app.route('/sites', methods=['POST'])
+def add_site():
+    new_site = request.get_json()
+    conn = get_db_connection()
+    conn.execute('''
+        INSERT INTO sites (link)
+        VALUES (?)
+    ''', (new_site['link'],))
+    conn.commit()
+    conn.close()
+    return '', 201
+
+@app.route('/reviews', methods=['POST'])
+def add_review():
+    new_review = request.get_json()
+    conn = get_db_connection()
+    conn.execute('''
+        INSERT INTO reviews (user_id, link, review)
+        VALUES (?, ?, ?)
+    ''', (new_review['user_id'], new_review['link'], new_review['review']))
+    conn.commit()
+    conn.close()
+    return '', 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
